43734243f3fd17272c1fd4bf528bb2bcc9987323
Elias <Graue421@newschool.edu>
2020-05-07 12:24:44 +0200

    first commit

---
+++ scripts/main.js
@@ -0,0 +1,32 @@
+var projects =[
+
+{	
+	"name": "SingBird",
+	"pic": "<img src='img/singbird_website_cover.png'>",
+	"detailPage": "'<a scr=''>",
+	"descr": "<p>SingBird: 05/18 - present</p>",
+
+
+},
+
+{	
+	"name": "Eye",
+	"pic": "<img src='img/eyeWebsiteCoverNoLogo.png'>",
+	"detailPage": "'<a scr=''>",
+	"descr": "<p>Glas: 05/19 </p>",
+
+},
+
+]
+
+for(var i=0; i<projects.length; i++){
+	var projectSummary = $('<div/>', {
+			'id': 'aProject'
+		})
+		.data('project', projects[i])
+		.html(projects[i].pic + projects[i].descr)
+		.click(function(){ URL(google.com); });
+
+	$('.container').append(projectSummary)
+}
+
